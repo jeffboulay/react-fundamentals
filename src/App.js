0f40d88d7e40e5fca4731dff426b0683fd2da0df
Jeff Boulay <BoulayJ@DNB.com>
2018-01-31 13:57:48 -0600

    created a form to update the name using ref and change handler

--- src/App.js
+++ src/App.js
@@ -9,9 +9,16 @@ class App extends Component {
       name:'Jeff'
     }
   }
+
+  update(e) {
+    this.setState({ name: e.target.value })
+  }
   render() {
     return (
       <div className="App">
+        <form>
+          <input type="name" ref='name' onChange={this.update.bind(this)} />
+        </form>
         <Helloword name={this.state.name}></Helloword>
       </div>
     );
